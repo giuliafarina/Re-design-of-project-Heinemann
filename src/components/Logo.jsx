5d92894cc198606fccfe8297573bb8a82086c636
= <=>
2021-05-27 11:57:49 +0200

    add logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+function Logo() {
+    return (<div>
+        <img class="logo" src="images/signatur-red.png">
+
+        </img>
+    </div>);
+};
+export default Logo;
