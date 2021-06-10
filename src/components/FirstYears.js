bf2090929677db2e10dfb94913a446f7f8045bc9
= <=>
2021-06-10 10:07:22 +0200

    add tabs

--- src/components/FirstYears.js
+++ src/components/FirstYears.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function FirstYears() {
-    return <p>FirstYears</p>
+    return <p>His first yers were influenced by `10 and `20ies artists</p>
 }
 
 export default FirstYears;
